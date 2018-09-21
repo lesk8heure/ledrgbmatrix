04a8146fbba73201bf6e19e6d1e3776b18497a54
lesk8heure <rom.laplace@gmail.com>
2018-09-21 15:24:56 +0200

    Update ledrgbmatrix.mk

--- PACKAGES/ledrgbmatrix/ledrgbmatrix.mk
+++ PACKAGES/ledrgbmatrix/ledrgbmatrix.mk
@@ -13,10 +13,6 @@ define LEDRGBMATRIX_BUILD_CMDS
     $(MAKE) CC=$(TARGET_CC) CXX=$(TARGET_CXX) AR=$(TARGET_AR) LD="$(TARGET_LD)" -C $(@D) all
 endef
 
-#define LEDRGBMATRIX_INSTALL_STAGING_CMDS
- #   $(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install
-#endef
-
 define LEDRGBMATRIX_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/autorun/demo $(TARGET_DIR)/usr/bin
     $(INSTALL) -D -m 0755 $(@D)/lib/librgbmatrix.so.1 $(TARGET_DIR)/usr/lib
